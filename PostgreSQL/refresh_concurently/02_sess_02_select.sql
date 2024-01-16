9c1636dee21b7ae5b998eb748255573e5af150cd
Lukáš Flek <lukas.flek>
2024-01-16 14:40:11 +0100

    refresh mv

--- PostgreSQL/refresh_concurently/02_sess_02_select.sql
+++ PostgreSQL/refresh_concurently/02_sess_02_select.sql
@@ -1,11 +1,15 @@
---takeaways (
---	- assumption: the dml transaction is not comitted
---	- refresh mv WITHOUT concurently
---		- tbl: tbl is not locked (is reachable); new rec not visible
---		- mv: mv is locked (not reachable); gets reachable after commit
---	- refresh mv WITH concurently
---		- tbl: tbl is not locked (is reachable); new rec not visible
---		- mv: mv is not locked (is reachable); new recs are not visible (mv provides data valid before refresh), new data are provided after commit
+--takeaways
+--	- assumption: the "main" dml transaction has not been comitted yet
+--	- tbl
+--		- tbl is NOT locked (is reachable)
+--		- new rec NOT visible (until the main transaction is committed) 
+--	- mv
+--		- refresh WITHOUT concurently
+--			- mv is locked (not reachable until the main transaction is committed)
+--		- refresh WITH concurently
+--			- mv is NOT locked (is reachable)
+--			- exposes data valid before the refresh
+--			- new data are exposed after the main transaction is committed
 SELECT * FROM lfl_test.mv_refresh_01_tbl;
 
 SELECT * FROM lfl_test.mv_refresh_02_mv;
